48ec9dc843e2bc100a9f8ee6602c822274c61bf7
yogurtsea <sylvia821120@gmail.com>
2018-01-29 15:16:51 +0800

    implement data energy

--- kfusion/include/kfusion/warp_field.hpp
+++ kfusion/include/kfusion/warp_field.hpp
@@ -57,7 +57,8 @@ namespace kfusion
         void energy_data(const std::vector<Vec3f> &canonical_vertices,
                           const std::vector<Vec3f> &canonical_normals,
                           const std::vector<Vec3f> &live_vertices,
-                          const std::vector<Vec3f> &live_normals);
+                          const std::vector<Vec3f> &live_normals,
+                          const Intr& intr);
         void energy_reg(const std::vector<std::pair<kfusion::utils::DualQuaternion<float>,
                 kfusion::utils::DualQuaternion<float>>> &edges);
 
@@ -67,6 +68,7 @@ namespace kfusion
 
         utils::DualQuaternion<float> DQB(const Vec3f& vertex) const;
         utils::DualQuaternion<float> DQB(const Vec3f& vertex, const std::vector<double*> epsilon) const;
+        utils::DualQuaternion<float> DQB_r(const Vec3f& vertex, const float weights[KNN_NEIGHBOURS], const unsigned long knn_indices_[KNN_NEIGHBOURS]) const; // [Minhui 2018/1/28]
         void update_nodes(const double *epsilon);
 
         void getWeightsAndUpdateKNN(const Vec3f& vertex, float weights[KNN_NEIGHBOURS]) const;
@@ -82,6 +84,8 @@ namespace kfusion
         void setWarpToLive(const Affine3f &pose);
         std::vector<float>* getDistSquared() const;
         std::vector<size_t>* getRetIndex() const;
+
+        std::vector<cv::Vec3f> live_vertices_;
 
     private:
         std::vector<deformation_node>* nodes_;
